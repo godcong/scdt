29c8f07a42214dd8a72f9e725561294fd869fb1c
godcong <jumbycc@163.com>
2020-06-23 15:59:10 +0800

    developing

--- message.go
+++ message.go
@@ -1,11 +1,16 @@
 package scdt
 
+type RequestType uint32
+type MessageProcess uint32
+type RequestStatus uint32
+
 type Message struct {
-	Version Version
-	Length  uint64
-	Session uint32
-	//Type       Type
-	//TypeDetail TypeDetail
-	//Status     Status
-	Data []byte
+	Version  Version
+	Length   uint64
+	Session  uint32
+	Type     RequestType
+	Process  MessageProcess
+	Status   RequestStatus
+	CheckSum []byte
+	Data     []byte
 }
